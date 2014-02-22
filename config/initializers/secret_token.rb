175b2fdaede0c10521a6ade8a9520a615cf056e5
unknown <Artem@Pasta.(none)>
2014-02-21 16:16:30 -0800

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Loom::Application.config.secret_key_base = 'bb596b5f6b7c7e3bb6d518a1b1a734eb61de2446a7a5280ad620ad97ff4f948055ed55c15503d7ef35e42a6fb49819ae455465b7b7818c3fe5c10fb80f9e8985'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Loom::Application.config.session_store :cookie_store, key: '_loom_session'
